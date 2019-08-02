f592890b88cf6f603be35e09954d63f27ea80c9b
Dong_Hyck_Na <43693001+agvx124@users.noreply.github.com>
2019-08-02 15:01:10 +0900

    무한스크롤 기능 구현
    In MainActivity.java

--- app/src/main/java/org/techtown/betweenus_android/view/activity/MainActivity.java
+++ app/src/main/java/org/techtown/betweenus_android/view/activity/MainActivity.java
@@ -1,8 +1,12 @@
 package org.techtown.betweenus_android.view.activity;
 
 import android.os.Bundle;
+import android.os.Handler;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import org.techtown.betweenus_android.R;
 import org.techtown.betweenus_android.base.BaseActivity;
@@ -19,6 +23,11 @@ public class MainActivity extends BaseActivity<MainActivityBinding> {
 
     private List<Study> studies;
     private MainViewModel mainViewModel;
+    MainListAdapter mainListAdapter = new MainListAdapter(studies, this);
+
+    // List 에 넣기 위한 index 선언
+    int index = 0;
+    boolean isLoading = false;
 
     @Override
     protected int layoutId() {
@@ -31,9 +40,53 @@ public class MainActivity extends BaseActivity<MainActivityBinding> {
 
         initViewModel();
         binding.studyRecyclerView.setAdapter(new MainListAdapter(studies, this));
+        initScrollListener();
     }
 
     private void initViewModel() {
         mainViewModel = ViewModelProviders.of(this, new ViewModelFactory(this)).get(MainViewModel.class);
+    }
+
+    private void initScrollListener() {
+
+        binding.studyRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                LinearLayoutManager linearLayoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+
+                if (!isLoading) {
+                    if (linearLayoutManager != null && linearLayoutManager.findLastCompletelyVisibleItemPosition() == lostFoundList.size() - 1) {
+                        if (index % 10 == 0) {
+                            loadMore();
+                        }
+                        isLoading = true;
+                    }
+                }
+            }
+        });
+    }
+
+    // 무한 스크롤 기능 구현
+    private void loadMore() {
+        studies.add(null);
+        mainListAdapter.notifyItemInserted(studies.size() - 1);
+
+        Handler handler = new Handler();
+        handler.postDelayed( ()-> {
+
+            studies.remove(studies.size() - 1);
+            int scrollPosition = studies.size();
+            mainListAdapter.notifyItemRemoved(scrollPosition);
+
+            isLoading = false;
+
+        }, 1000);
     }
 }
