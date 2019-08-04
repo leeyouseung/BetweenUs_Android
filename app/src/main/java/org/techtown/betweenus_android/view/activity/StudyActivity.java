f51dbe8bb6e4567def0ca0c59722dbb6cd35b778
rjsdnqkr1 <39177603+rjsdnqkr1@users.noreply.github.com>
2019-08-04 23:18:32 +0900

    study 신청창 어느정도

--- app/src/main/java/org/techtown/betweenus_android/view/activity/StudyActivity.java
+++ app/src/main/java/org/techtown/betweenus_android/view/activity/StudyActivity.java
@@ -1,12 +1,18 @@
 package org.techtown.betweenus_android.view.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.bumptech.glide.Glide;
 
 import org.techtown.betweenus_android.R;
 import org.techtown.betweenus_android.base.BaseActivity;
 import org.techtown.betweenus_android.databinding.StudyActivityBinding;
+import org.techtown.betweenus_android.model.Study;
 
 public class StudyActivity extends BaseActivity<StudyActivityBinding> {
+
+    private Study study;
 
     @Override
     protected int layoutId() {
@@ -16,5 +22,35 @@ public class StudyActivity extends BaseActivity<StudyActivityBinding> {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        initIntent();
+
+        initText();
+
+        clickEvent();
+    }
+
+    private void initIntent() {
+        Intent intent = getIntent();
+        study = (Study) intent.getSerializableExtra("study");
+    }
+
+    private void initText() {
+        binding.startTermText.setText(study.getStartTerm().split(" ")[0]);
+        binding.endTermText.setText(study.getEndTerm().split(" ")[0]);
+        binding.startTimeText.setText(study.getStartTime());
+        binding.endTimeText.setText(study.getEndTime());
+        binding.memberIdText.setText(study.getMemberId());
+        binding.personnelText.setText(study.getCurrentPerson() + " / " + study.getPersonnel());
+        binding.placeText.setText(study.getLocation());
+        binding.studyTitle.setText(study.getTitle());
+        binding.studyDescription.setText(study.getDescription());
+        Glide.with(this).load(study.getImgs().get(0)).into(binding.studyImageview);
+    }
+
+    private void clickEvent() {
+        binding.applyBtn.setOnClickListener(v -> {
+
+        });
     }
 }
