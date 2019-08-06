749709f7fc4301ad36c7da3ac0030e136473d5a6
Dong_Hyck_Na <43693001+agvx124@users.noreply.github.com>
2019-08-06 18:05:28 +0900

    aaa

--- app/src/main/java/org/techtown/betweenus_android/view/activity/MyPageActivity.java
+++ app/src/main/java/org/techtown/betweenus_android/view/activity/MyPageActivity.java
@@ -3,6 +3,8 @@ package org.techtown.betweenus_android.view.activity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.bumptech.glide.Glide;
+
 import org.techtown.betweenus_android.R;
 import org.techtown.betweenus_android.base.BaseActivity;
 import org.techtown.betweenus_android.databinding.MypageActivityBinding;
@@ -19,12 +21,21 @@ public class MyPageActivity extends BaseActivity<MypageActivityBinding> {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        CurrentUser currentUser = new CurrentUser(this, "betweenUs.db", null, 2);
+        getMyInfo();
+    }
 
+    private void getMyInfo() {
+        CurrentUser currentUser = new CurrentUser(this, "betweenUs.db", null, 2);
         Member myInfo = currentUser.getResult();
 
-        Log.d("Info", myInfo.getName());
-
+        Log.d("imgTag", myInfo.getProfileimg());
+        if (!myInfo.getProfileimg().equals("null")) {
+            Log.d("imgTag", "Pass");
+            Glide.with(this).load(myInfo.getProfileimg()).into(binding.userImageview);
+        }
         binding.nameText.setText(myInfo.getName());
+        binding.schoolText.setText(myInfo.getSchool());
+        binding.gradeText.setText(String.valueOf(myInfo.getGrade()));
+        binding.classText.setText(String.valueOf(myInfo.getSchoolClass()));
     }
 }
