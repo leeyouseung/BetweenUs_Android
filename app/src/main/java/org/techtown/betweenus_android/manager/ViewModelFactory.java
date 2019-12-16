b41d3dea279d281b60a030bf89fab7de3e6a1680
Leeyuseung <38912467+leeyouseung@users.noreply.github.com>
2019-12-16 19:08:45 +0900

    Update ViewModelFactory.java

--- app/src/main/java/org/techtown/betweenus_android/manager/ViewModelFactory.java
+++ app/src/main/java/org/techtown/betweenus_android/manager/ViewModelFactory.java
@@ -17,7 +17,6 @@ import org.techtown.betweenus_android.viewmodel.StudyApplyViewModel;
 import org.techtown.betweenus_android.viewmodel.StudyViewModel;
 
 /**
- * @author 우주 최강 천재 건우
  * ViewModel 추가 시킬때 마다 create에 추가 시켜 줘야됨
  */
 public class ViewModelFactory extends ViewModelProvider.NewInstanceFactory {
